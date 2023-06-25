8e934bb979180b9524813032d92560ec4a530361
aman <amanrajaryanji12@gmail.com>
2023-06-25 16:39:43 +0530

    first

--- main.js
+++ main.js
@@ -1,24 +1,41 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as BABYLON from 'babylonjs'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+
+
+const canvas = document.getElementById('renderCanvas');
+
+const engine = new BABYLON.Engine(canvas);
+
+const createScene = function () {
+  
+
+const scene = new BABYLON.Scene(engine , {antialias : true})
+
+
+const box = new BABYLON.MeshBuilder.CreateTube('tube' , 
+{path : BABYLON.Vector3[
+  (0 , 0 , 0),
+  (3 ,3 , 3),
+  (6, 6 ,6),
+  (9, 9 , 9)
+  (12 , 12 , 12)
+]})
+
+
+return scene;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
