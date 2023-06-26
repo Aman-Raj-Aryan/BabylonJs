4e035b24aab684d780b7bbafaf9adb9f60400545
aman <amanrajaryanji12@gmail.com>
2023-06-26 18:13:07 +0530

    hm created

--- main.js
+++ main.js
@@ -11,28 +11,44 @@ var createScene = function(){
 
  var scene = new BABYLON.Scene(engine);
 
-var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
+var camera = new BABYLON.ArcRotateCamera('cam' , 1 , 1 , 20 , new BABYLON.Vector3.Zero() , scene);
+
  camera.setTarget(BABYLON.Vector3.Zero());
-         camera.attachControl                 
+         camera.attachControl(true)             
  var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
  var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene, false, BABYLON.Mesh.FRONTSIDE);
                                     
        sphere.position.y = 1;
                                                      
-                                          var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
-                                                               
-                        return scene;
-                                                                }
+var ground = BABYLON.MeshBuilder.CreateGroundFromHeightMap('ground' , 'assets/hm.png' , {
+        width : 10,
+        height : 10,
+        subdivisions : 20,
+
+
+
+})
+
+                                                             
+  return scene;
+     }
                                                                     
-                          var scene = createScene();
+         var scene = createScene();
                                                                
-                       engine.runRenderLoop(function(){
-                                              scene.render();
+ engine.runRenderLoop(function(){
+        scene.render();
                                                                             });
                                                                             // the canvas/window resize event handler
-          window.addEventListener('resize', function(){
+ window.addEventListener('resize', function(){
                                                            engine.resize();
                     });
+
+
+
+
+
+
+
 
 
 
