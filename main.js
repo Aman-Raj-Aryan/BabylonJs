807acb9a87c514dc0e29d89714c9c24de9607a08
aman <amanrajaryanji12@gmail.com>
2023-06-26 13:53:44 +0530

    f

--- main.js
+++ main.js
@@ -2,29 +2,51 @@ import * as BABYLON from 'babylonjs'
 
 
 
+// Get the canvas DOM element
+var canvas = document.getElementById('renderCanvas');
+// Load the 3D engine
+var engine = new BABYLON.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true});
+// CreateScene function that creates and return the scene
+var createScene = function(){
 
-const canvas = document.getElementById('renderCanvas');
+ var scene = new BABYLON.Scene(engine);
 
-const engine = new BABYLON.Engine(canvas);
+var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
+ camera.setTarget(BABYLON.Vector3.Zero());
+         camera.attachControl                 
+ var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+ var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene, false, BABYLON.Mesh.FRONTSIDE);
+                                    
+       sphere.position.y = 1;
+                                                     
+                                          var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
+                                                               
+                        return scene;
+                                                                }
+                                                                    
+                          var scene = createScene();
+                                                               
+                       engine.runRenderLoop(function(){
+                                              scene.render();
+                                                                            });
+                                                                            // the canvas/window resize event handler
+          window.addEventListener('resize', function(){
+                                                           engine.resize();
+                    });
 
-const createScene = function () {
+
+
   
 
-const scene = new BABYLON.Scene(engine , {antialias : true})
 
 
-const box = new BABYLON.MeshBuilder.CreateTube('tube' , 
-{path : BABYLON.Vector3[
-  (0 , 0 , 0),
-  (3 ,3 , 3),
-  (6, 6 ,6),
-  (9, 9 , 9)
-  (12 , 12 , 12)
-]})
 
 
-return scene;
-}
+
+
+
+
+
 
 
 
